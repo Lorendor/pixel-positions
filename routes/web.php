b76c1eb58a7afb65a9d5c9715e5601999376d746
lorenbjelokosic <l.bjelokosic@gmail.com>
2025-09-02 05:04:24 +0200

    url , blade changes ,logout

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\JobController::class,'index']);
 
 Route::get('/jobs/create', [\App\Http\Controllers\JobController::class,'create'])->middleware('auth');
-Route::get('/jobs', [\App\Http\Controllers\JobController::class,'store'])->middleware('auth');
+Route::post('/jobs', [\App\Http\Controllers\JobController::class,'store'])->middleware('auth');
 
 Route::get('/search', \App\Http\Controllers\SearchController::class);
 Route::get('/tags/{tag:name}', \App\Http\Controllers\TagController::class);
